b9e32d4903359b854c769cb213aa6b1262904d70
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-02 23:29:46 +0530

    form events

---
+++ 25 JavaScript Part 10/form events/app.js
@@ -0,0 +1,7 @@
+let form = document.querySelector("form");
+
+form.addEventListener('submit', function(event) {
+    alert('form submitted');
+    console.log("form submitted");
+    // event.preventDefault;
+});
