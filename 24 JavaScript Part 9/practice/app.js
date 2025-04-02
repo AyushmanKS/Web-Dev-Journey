5320011005b172a7e77a8197df4e730c078c1898
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-02 16:05:23 +0530

    dom manipulation

---
+++ 24 JavaScript Part 9/practice/app.js
@@ -0,0 +1,12 @@
+let para1 = document.createElement('p');
+para1.innerText = ' Hey im first paragraph!';
+document.querySelector('body').append(para1);
+
+let h3 = document.createElement('h3');
+h3.innerText = ' Hey im first h3 heading!';
+document.querySelector('body').append(h3);
+
+
+
+
+
