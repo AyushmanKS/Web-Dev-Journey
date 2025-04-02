f4d8b69e303ae94dc8f1850a836fe52a786e1453
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-02 10:01:18 +0530

    javascript every method

--- 17 JavaScript Part 2/app.js
+++ 17 JavaScript Part 2/app.js
@@ -1,5 +1,17 @@
-console.log("Learning JavaScript");    
+console.log("Learning JavaScript");
 
-alert("Something is wrong!")
+alert("Something is wrong!");
 
-prompt("Enter your name?")
+// let name = prompt("Enter your name?");
+// console.log("Hello, " + name);
+
+// let nums = [1, 2, 3, 4, 5];
+
+// let final = nums.reduce((res, el) => res + el, 0);
+// console.log(final);
+
+let nums = [10, 20, 30, 50, 50];
+
+let ans = nums.every((el) => el%10 == 0);
+
+console.log(ans);
