1140abc7d461f90b94bced77acdc5519059adf53
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-04 20:02:26 +0530

    event delegation

--- 26 JavaScript Mini Project/todo/app.js
+++ 26 JavaScript Mini Project/todo/app.js
@@ -21,10 +21,17 @@ btn.addEventListener("click", function() {
     }
 });
 
-let delBtns = document.querySelectorAll(".delete");
-for(delBtn of delBtns) {
-    delBtn.addEventListener("click", function() {
-        let par = this.parentElement;
-        par.remove();
-    });
-}
+ul.addEventListener("click", function(event) {
+    if(event.target.nodeName == 'BUTTON') {
+        let listItem = event.target.parentElement;
+        listItem.remove();
+    }
+});
+
+// let delBtns = document.querySelectorAll(".delete");
+// for(delBtn of delBtns) {
+//     delBtn.addEventListener("click", function() {
+//         let par = this.parentElement;
+//         par.remove();
+//     });
+// }
