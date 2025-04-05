76fdafa2ff2c6aff241f27bb426f1641949e38f2
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-05 09:30:20 +0530

    level updates

--- 26 JavaScript Mini Project/simon says game/app.js
+++ 26 JavaScript Mini Project/simon says game/app.js
@@ -29,19 +29,38 @@ function userFlash(btn) {
 }
 
 function levelUp() {
+    userSeq = [];
     level++;
     h2.innerText = `Level ${level}`;
 
     let randIdx = Math.floor(Math.random()*3);
     let randColor = btns[randIdx];
     let randbtn = document.querySelector(`.${randColor}`); 
+    gameSeq.push(randColor);
     gameFlash(randbtn);
+}
+
+function checkAns(idx) {
+
+    if(userSeq[idx] == gameSeq[idx]) {
+        if(userSeq.length == gameSeq.length) {
+            setTimeout(levelUp, 1000);
+        }
+    } else {
+        h2.innerText = `Game over! Press any key to start again.`;
+    }
 }
 
 function btnPress() {
     console.log(`${this} button was pressed`);
     let btn = this;
     userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    console.log(`-------------------------------------${userColor}`);
+    userSeq.push(userColor);
+
+    checkAns(userSeq.length-1);
 }
 
 let allBtns = document.querySelectorAll(".btn");
