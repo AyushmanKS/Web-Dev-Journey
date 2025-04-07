686b0df7daaa7ce7387f233850a85a10e00c32ed
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-07 19:03:07 +0530

    json format

---
+++ 28 JavaScript Part 12/api/app.js
@@ -0,0 +1,8 @@
+let jsonRes = '{"fact":"Cats are subject to gum disease and to dental caries. They should have their teeth cleaned by the vet or the cat dentist once a year.","length":133}';
+
+let valiRes = JSON.parse(jsonRes);
+
+let student = {
+    name : "Ayushman",
+    marks: '99',
+};
