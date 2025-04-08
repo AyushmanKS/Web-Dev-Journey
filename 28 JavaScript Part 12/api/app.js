fb0dee8952667040ba697efd5111c4c4dbd0b29b
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-08 15:41:56 +0530

    async await api calls

--- 28 JavaScript Part 12/api/app.js
+++ 28 JavaScript Part 12/api/app.js
@@ -7,15 +7,32 @@
 //     marks: '99',
 // };
 
-let url = "https://catfact.ninja/fact";
-fetch(url)
-.then((response)=> {
-    console.log(response);
+// let url = "https://catfact.ninja/fact";
+// fetch(url)
+// .then((response)=> {
+//     console.log(response);
 
-    response.json().then((data)=> {
-        console.log(data);
-    });
-})
-.catch((err)=> {
-    console.log("Error: ",err);
-});
+//     response.json().then((data)=> {
+//         console.log(data);
+//     });
+// })
+// .catch((err)=> {
+//     console.log("Error: ",err);
+// });
+
+let url = "https://catfact.ninja/fact";
+
+async function getFacts() {
+    try {
+        let res = await fetch(url);
+        let data = await res.json();
+        console.log(data.fact);
+    }
+    catch(e) {
+        console.log("Error: ",e);
+    }
+
+    console.log("Function ends here");
+}
+
+getFacts();
