ea6910c39ab500a5c69075fe3e0c5d6f7aee0c08
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-08 15:35:25 +0530

    api calls

--- 28 JavaScript Part 12/api/app.js
+++ 28 JavaScript Part 12/api/app.js
@@ -1,8 +1,21 @@
-let jsonRes = '{"fact":"Cats are subject to gum disease and to dental caries. They should have their teeth cleaned by the vet or the cat dentist once a year.","length":133}';
+// let jsonRes = '{"fact":"Cats are subject to gum disease and to dental caries. They should have their teeth cleaned by the vet or the cat dentist once a year.","length":133}';
 
-let valiRes = JSON.parse(jsonRes);
+// let valiRes = JSON.parse(jsonRes);
 
-let student = {
-    name : "Ayushman",
-    marks: '99',
-};
+// let student = {
+//     name : "Ayushman",
+//     marks: '99',
+// };
+
+let url = "https://catfact.ninja/fact";
+fetch(url)
+.then((response)=> {
+    console.log(response);
+
+    response.json().then((data)=> {
+        console.log(data);
+    });
+})
+.catch((err)=> {
+    console.log("Error: ",err);
+});
