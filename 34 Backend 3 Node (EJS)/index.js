2d503aac248a3d3268e25c7f10589db893b323c5
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-12 17:14:09 +0530

    using ejs

---
+++ 34 Backend 3 Node (EJS)/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const port = 8000;
+
+app.set("view engine","ejs");
+
+app.get('/',(req, res)=>{
+    res.render("home.ejs");
+});
+
+app.listen(port, ()=> {
+    console.log(`listening to port ${port}`);
+});
