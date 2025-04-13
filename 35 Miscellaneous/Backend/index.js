38e9cf5eb4dd40a9dac8ae964bb998be402851a7
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-13 12:50:27 +0530

    handling post requests

--- 35 Miscellaneous/Backend/index.js
+++ 35 Miscellaneous/Backend/index.js
@@ -3,12 +3,16 @@ const app = express();
 
 const port = 8080;
 
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
 app.get("/register", (req, resp)=>{
     let {user, password} = req.query;
     resp.send(`Standard get response, welcome ${user}`);
 });
 
 app.post("/register", (req, resp)=>{
+    console.log(req.body);
     resp.send("Standard post response");
 });
 
