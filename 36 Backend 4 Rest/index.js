ce5e6e83b18ae4b8483351070a05976614aa8e68
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-14 19:53:50 +0530

    quora project setup

---
+++ 36 Backend 4 Rest/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname,"views"));
+app.set(express.static(path.join(__dirname,"public")));
+
+const port = 8080;
+
+app.get("/",(req, resp)=>{
+    resp.send("server working!");
+});
+
+app.listen(port, ()=>{
+    console.log(`Listening to port: ${port}`);
+}); 
