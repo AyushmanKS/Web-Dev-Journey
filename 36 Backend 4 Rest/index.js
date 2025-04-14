db2895c27c5a3a87d13cfa0f1d75738fb6ba03b1
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-15 01:06:03 +0530

    patch request: edit posts

--- 36 Backend 4 Rest/index.js
+++ 36 Backend 4 Rest/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const {v4: uuidv4} = require("uuid");
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -49,6 +51,21 @@ app.get("/posts/:id",(req,resp)=>{
 
 app.get("/posts",(req, resp)=>{
     resp.render("index.ejs", {posts});
+});
+
+app.patch("/posts/:id",(req,resp)=>{
+    let {id} = req.params;
+    let post = posts.find((p)=> id === p.id);
+    let newContent = req.body.content;
+
+    post.content = newContent; 
+    resp.redirect("/posts");
+});
+
+app.get("/posts/:id/edit",(req,resp)=>{
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    resp.render("edit.ejs",{post});
 });
 
 app.listen(port, ()=>{
