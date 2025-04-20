bce4ad02798f6c236f7584717ebc5f095f6eab5b
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-20 16:35:27 +0530

    add template (ejs)

--- 44 Backend 5 (Node with sql)/index.js
+++ 44 Backend 5 (Node with sql)/index.js
@@ -2,6 +2,10 @@ const {faker} = require('@faker-js/faker');
 const mysql = require('mysql2');
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -47,8 +51,8 @@ app.get('/',(req,res)=>{
     try {
     connection.query(q, (err, result)=>{
     if(err) throw err;
-    console.log(result);
-    res.send(result);
+    let total = result[0]["count(*)"];
+    res.render("home.ejs",{total});
       });
     } catch(err) {
       console.log(err);
