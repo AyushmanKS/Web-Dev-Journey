c0b92ae14d144e2155d978a0e4536acf2af1268e
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-22 00:07:18 +0530

    mongoose setup

---
+++ 47 MongoDB Part 2/index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+main().then((res)=>{
+        console.log(res);
+    })
+    .catch(err => console.log("Connection successful!"));
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:2717/test");
+}
