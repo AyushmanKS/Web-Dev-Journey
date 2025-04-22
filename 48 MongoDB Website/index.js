b869b69609e3ee5789c9591ac5079db9bed75f0f
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-23 00:37:29 +0530

    edit route

--- 48 MongoDB Website/index.js
+++ 48 MongoDB Website/index.js
@@ -60,6 +60,13 @@ app.post("/chats", (req, res) => {
     });
 });
 
+// Edit route
+app.get("/chats/:id/edit",async (req,res)=>{
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs",{chat});
+});
+
 app.get("/",(req, res)=> {
     res.send("Root route is working!");
 });
