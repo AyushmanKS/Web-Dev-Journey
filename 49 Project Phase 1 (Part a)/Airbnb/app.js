81c0cc167fa03916ed4a83ad48d39846c1a69bd3
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-24 22:52:07 +0530

    listing model

--- 49 Project Phase 1 (Part a)/Airbnb/app.js
+++ 49 Project Phase 1 (Part a)/Airbnb/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const listing = require("../Airbnb/models/listing.js");
 
 main().then(()=> {
     console.log("Connected to DB");
@@ -11,6 +12,20 @@ main().then(()=> {
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
+
+app.get("/testListing",async (req,res)=> {
+    let sampleListing = new listing({
+        title: "My new Villa!",
+        description: "By the beach~~",
+        image: "",
+        price: 90000,
+        location: "Kanpur Uttar Pradesh",
+        country: "India"
+    });
+
+    await sampleListing.save();
+    res.send(`Saved to DB: ${sampleListing}`);
+});
 
 app.get("/",(req, res)=>{
     res.send("Hi im root! ");
---
+++ 49 Project Phase 1 (Part a)/Airbnb/models/listing.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        type: String,
+        default: "https://images.unsplash.com/photo-1730800329058-dbb9917a7ec7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set:(v) => v === "" 
+        ? "https://images.unsplash.com/photo-1730800329058-dbb9917a7ec7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+        : v,  
+    },
+    price: Number,
+    location: String,
+    country: String
+});
+
+const Listing = mongoose.model("Listing", listSchema);
+module.exports = Listing;
