bca3d091ac39a84538ec054d3f58dfe43aeda8a0
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-24 23:39:39 +0530

    initialize database

---
+++ 49 Project Phase 1 (Part a)/Airbnb/init/index.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+main().then(()=>{
+    console.log("Successfully connected to db");
+    return initDB();
+}).catch((err)=>{
+    console.log(`Error connecting to db: ${err}`);
+});
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+}
+
+const initDB = async()=> {
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("Data is initialized");
+};
