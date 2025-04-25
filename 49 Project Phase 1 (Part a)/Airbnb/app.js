4d69823224a9003fbb24f32b7920a7c2b59ba2b3
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-25 18:44:06 +0530

    edit and update route

--- 49 Project Phase 1 (Part a)/Airbnb/app.js
+++ 49 Project Phase 1 (Part a)/Airbnb/app.js
@@ -3,10 +3,12 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("../Airbnb/models/listing.js");
 const path = require("path"); 
+const methodOverride = require("method-override");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 main().then(()=> {
     console.log("Connected to DB");
@@ -54,6 +56,20 @@ app.get("/listings/:id",async (req,res)=>{
 app.post("/listings",async (req,res)=>{
     const newListing = new Listing(req.body.listing);
     await newListing.save();
+    res.redirect("/listings");
+});
+
+// Edit route
+app.get("/listings/:id/edit", async (req,res)=>{
+    let {id} = req.params;
+    let listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+});
+
+// Update route
+app.put("/listings/:id",async (req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect("/listings");
 });
 
