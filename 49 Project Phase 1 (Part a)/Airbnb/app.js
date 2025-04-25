f005b60353b335b339abd4d4104399bbe1d005ab
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-25 13:58:04 +0530

    read/show route

--- 49 Project Phase 1 (Part a)/Airbnb/app.js
+++ 49 Project Phase 1 (Part a)/Airbnb/app.js
@@ -6,6 +6,7 @@ const path = require("path");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
 
 main().then(()=> {
     console.log("Connected to DB");
@@ -31,9 +32,17 @@ async function main() {
 //     res.send(`Saved to DB: ${sampleListing}`);
 // });
 
+// Index route
 app.get("/listings",async (req,res) => {
     let allListings = await Listing.find({});
     res.render("listings/index.ejs",{allListings});
+});
+
+// Show route
+app.get("/listings/:id",async (req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
 });
 
 app.get("/",(req, res)=>{
