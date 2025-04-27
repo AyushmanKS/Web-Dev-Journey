37d3aacafbe2adb3c3be48f70cc1b58ecaa81589
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-27 14:03:43 +0530

    custom errors

---
+++ 52 Backend 7 Errors/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const ExpressError = require("./ExpressError");
+
+const checkToken = (req,res,next)=> {
+    let {token} = req.query;
+    if(token === "giveaccess") {
+        next();
+    }
+    throw new ExpressError(401, "ACCESS DENIED");
+};
+
+app.get("/api",checkToken,(req,res)=>{
+    res.send("data");
+});
+
+app.get('/random',(req,res)=>{
+    res.send("This is a random page");
+});
+
+app.get('/',(req,res)=>{
+    res.send("Hi i am root!");
+})
+
+app.listen(8080, ()=>{
+    console.log("Listening to port");
+});
