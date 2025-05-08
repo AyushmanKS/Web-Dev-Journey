62ae8ea6bbd7b8836aa347f18fcb2d8f93569393
AyushmanKS <ayushmanks1845@gmail.com>
2025-05-08 13:20:53 +0530

    login user

--- 49 Project Phase 1 (Part a)/Airbnb/routes/user.js
+++ 49 Project Phase 1 (Part a)/Airbnb/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require('../utils/wrapAsync');
+const passport = require('passport');
 
 router.get("/signup",(req,res) => {
     res.render("users/signup.ejs");
@@ -18,8 +19,18 @@ router.post("/signup",wrapAsync( async (req,res)=>{
     }
     catch(err) {
         req.flash("error",err.message);
-        res.redirect("/signup");
+        res.redirect("/login");
     }
 }));
+
+// login routes
+router.get("/login",(req,res)=>{
+    res.render("users/login.ejs");
+});
+
+router.post("/login",passport.authenticate("local",{failureRedirect: '/login', failureFlash: true,}), async(req,res)=> {
+    res.redirect("/listings");
+
+});
 
 module.exports = router;
