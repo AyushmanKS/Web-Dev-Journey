376faa73183074899eaff80aacdcadcd15e1cc63
AyushmanKS <ayushmanks1845@gmail.com>
2025-05-11 00:30:15 +0530

    post login route handling

--- 49 Project Phase 1 (Part a)/Airbnb/middleware.js
+++ 49 Project Phase 1 (Part a)/Airbnb/middleware.js
@@ -1,6 +1,14 @@
 module.exports.isLoggedIn = (req, res, next)=> {
+    req.session.redirectUrl = req.originalUrl;
     if(!req.isAuthenticated()) {
         return res.redirect('/login');
     }
     next();
 }
+
+module.exports.saveRedirectUrl = (req,res,next)=> {
+    if(req.session.redirectUrl) {
+        res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+} 
